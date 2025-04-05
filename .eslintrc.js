27e6bafe9bf06e9b2b7650a4f36fe6c609d16de7
David Afonso <davafons@gmail.com>
2025-04-05 22:14:49 +0900

    Grammar cards

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
